688b7d9462cc16b7e07503518f3747a9dabdcba1
Mike <mluciatomey@gmail.com>
2024-06-04 20:04:49 +0200

    ShowExtraHoursFragment

---
+++ app/src/main/java/com/example/emplostaff2/ui/ShowExtraHours/ShowExtraHoursViewModel.java
@@ -0,0 +1,7 @@
+package com.example.emplostaff2.ui.ShowExtraHours;
+
+import androidx.lifecycle.ViewModel;
+
+public class ShowExtraHoursViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
